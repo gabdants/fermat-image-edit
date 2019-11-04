b55d59f4c26f4a891f55766a3e50c34e4f987835
GM-atteoni <pepematteoni@hotmail.com>
2019-11-04 00:06:41 -0300

    inicio custom image

--- src/app/custom-image/custom-image.component.ts
+++ src/app/custom-image/custom-image.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ImageService } from '../services/image/image-service';
+import { Variavel } from 'src/typings/variavel';
 
 @Component({
   selector: 'app-custom-image',
@@ -12,11 +13,23 @@ export class CustomImageComponent implements OnInit {
   constructor(public route: ActivatedRoute, private imageService: ImageService) { }
   titulo: string;
 
+  listaVariaveis: Variavel[];
+
   ngOnInit() {
-    let id = this.route.snapshot.params.id;
-    this.imageService.getImageById(id).subscribe(response => {
-      console.log(response);
+    // let id = this.route.snapshot.params.id;
+    // this.imageService.getImageById(id).subscribe(response => {
+    //   console.log(response);
+    // })
+
+    this.imageService.getImagensMock().subscribe((res) => {
+      console.log(res);
     })
+
+    this.imageService.getVariaveisMock().subscribe((res) => {
+      this.listaVariaveis = res;
+      
+    })
+
   }
   chamaPreview(){
 
--- src/app/services/image/image-service.ts
+++ src/app/services/image/image-service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Variavel } from 'src/typings/variavel';
 
 const API_URL = environment.apiUrl;
 
@@ -17,6 +18,14 @@ export class ImageService {
     });
 
     return this.http.get<any>(`${API_URL}:8181/image/approvedImages?imageId=${id}`, {headers});
+  }
+
+  getImagensMock(){
+    return this.http.get<any>('http://localhost:3000/imagens');
+  }
+
+  getVariaveisMock(){
+    return this.http.get<Variavel[]>('http://localhost:3000/variaveis');
   }
 
 }
